e3729b5997330e5d1ce661555770fb2cef2bb7bc
bobbintb <bobbintb@gmail.com>
2025-11-05 21:51:41 -0700

    Update simple-screenshot_test.py

--- tests/simple-screenshot_test.py
+++ tests/simple-screenshot_test.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Page
 @pytest.mark.describe("DIRT Tabulator Page Screenshot")
 def test_tabulator_screenshot(page: Page):
     # Go to the page
-    page.goto("http://localhost/dirt-tabulator.php")
+    page.goto("http://localhost/index.php")
 
     # Wait for 10 seconds to allow the page to load
     page.wait_for_timeout(10000)
